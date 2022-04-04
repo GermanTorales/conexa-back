dc81eafe24adb8f6d35e5d3bdd02c4c28a21bb54
German Torales <germantorales73@gmail.com>
2022-04-04 20:35:42 -0300

    Create get photos validation

--- src/validations/jsonplaceholder.validation.js
+++ src/validations/jsonplaceholder.validation.js
@@ -6,3 +6,10 @@ export const getPosts = {
     size: Joi.number().default(10),
   }),
 };
+
+export const getPhotos = {
+  query: Joi.object().keys({
+    page: Joi.number().default(0),
+    size: Joi.number().default(10),
+  }),
+};
