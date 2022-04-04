ee17a9b854c5e4b893dc072050a6af7b0d1a1211
German Torales <germantorales73@gmail.com>
2022-04-04 20:35:30 -0300

    Create photos route

--- src/routes/v1/jsonplaceholder.routes.js
+++ src/routes/v1/jsonplaceholder.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { JsonplaceholderController } from '../../controllers';
 import { auth, validate } from '../../middlewares';
-import { getPosts } from '../../validations';
+import { getPhotos, getPosts } from '../../validations';
 
 const router = express.Router();
 
 router.use('/jsonplaceholder', router);
 
 router.get('/posts', auth(), validate(getPosts), JsonplaceholderController.handleGetPosts);
+router.get('/photos', auth(), validate(getPhotos), JsonplaceholderController.handleGetPhotos);
 
 export default router;
