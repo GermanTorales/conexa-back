6b6a8e864447171d131e4537beef2d55aa153c78
German Torales <germantorales73@gmail.com>
2022-04-04 16:29:00 -0300

    Config server

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 import config from './config';
 import logger from './logger';
 import morganConfig from './morgan';
-import passport from './passport';
+export * from './passport';
 
-export { config, morganConfig, logger, passport };
+export { config, morganConfig, logger };
